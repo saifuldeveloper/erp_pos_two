b05864a3a6188a22b8aa5985f66951f8ddb822a4
saifuldeveloper <mdsaiful77336@gmail.com>
2024-11-17 22:58:03 +0600

    error issue

--- app/Models/ProductPurchase.php
+++ app/Models/ProductPurchase.php
@@ -9,6 +9,6 @@ class ProductPurchase extends Model
     protected $table = 'product_purchases';
     protected $fillable =[
 
-        "purchase_id", "product_id", "product_batch_id", "variant_id", "imei_number", "qty", "recieved", "purchase_unit_id", "net_unit_cost", "discount", "tax_rate", "tax", "total"
+        "purchase_id", "product_id", "product_batch_id","selling_price", "variant_id", "imei_number", "qty", "recieved", "purchase_unit_id", "net_unit_cost", "discount", "tax_rate", "tax", "total"
     ];
 }
