e30fe0e3334eeb23937c878b4b6f1fd5001219f8
emon1432 <ovirajemon143@gmail.com>
2025-01-22 01:03:21 +0600

    Implement stock count edit and update functionality; adjust product variant quantities accordingly

--- resources/views/backend/stock_count/edit.blade.php
+++ resources/views/backend/stock_count/edit.blade.php
@@ -17,8 +17,8 @@
                                 <small>{{ trans('file.The field labels marked with * are required input fields') }}.</small>
                             </p>
                             {!! Form::open([
-                                'route' => 'stock-count.store',
-                                'method' => 'post',
+                                'route' => ['stock-count.update', $lims_stock_count->id],
+                                'method' => 'put',
                                 'files' => true,
                                 'id' => 'stock-count-form',
                             ]) !!}
@@ -29,7 +29,8 @@
                                             <div class="form-group">
                                                 <label>{{ trans('file.Date') }}</label>
                                                 <input type="text" name="created_at" class="form-control date"
-                                                    value="{{ date('d-m-Y') }}" placeholder="Choose date" />
+                                                    value="{{ date('m/d/Y', strtotime($lims_stock_count->created_at)) }}"
+                                                    placeholder="Choose date" />
                                             </div>
                                         </div>
                                         <div class="col-md-4">
@@ -39,7 +40,7 @@
                                                     data-live-search="true" title="Select warehouse...">
                                                     @foreach ($lims_warehouse_list as $warehouse)
                                                         <option value="{{ $warehouse->id }}"
-                                                            {{ $warehouse->is_default ? 'selected' : '' }}>
+                                                            {{ $warehouse->id == $lims_stock_count->warehouse_id ? 'selected' : '' }}>
                                                             {{ $warehouse->name }}</option>
                                                     @endforeach
                                                 </select>
@@ -58,15 +59,6 @@
                                                 @endif
                                             </div>
                                         </div>
-                                        <div class="col-md-12 mt-3">
-                                            <label>{{ trans('file.Select Product') }}</label>
-                                            <div class="search-box input-group">
-                                                <button class="btn btn-secondary"><i class="fa fa-barcode"></i></button>
-                                                <input type="text" name="product_code_name" id="lims_productcodeSearch"
-                                                    placeholder="Please type product code and select..."
-                                                    class="form-control" />
-                                            </div>
-                                        </div>
                                     </div>
                                     <div class="row mt-4">
                                         <div class="col-md-12">
@@ -79,10 +71,34 @@
                                                             <th>{{ trans('file.Code') }}</th>
                                                             <th>Current Quantity</th>
                                                             <th>{{ trans('file.Quantity') }}</th>
-                                                            <th><i class="dripicons-trash"></i></th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
+                                                        @foreach ($lims_stock_count->items as $key => $stock_count_product)
+                                                        <input type="hidden" name="product_id[]" value="{{ $stock_count_product->product_id }}" />
+                                                            <tr>
+                                                                <td>
+                                                                    {{ $stock_count_product->product->name }}
+                                                                </td>
+                                                                <td>
+                                                                    {{ $stock_count_product->item_code }}
+                                                                    <input type="hidden" name="product_code[]"
+                                                                        value="{{ $stock_count_product->item_code }}" />
+                                                                </td>
+                                                                <td>
+                                                                    {{ $stock_count_product->current_quantity }}
+                                                                    <input type="hidden" name="current_qty[]"
+                                                                        value="{{ $stock_count_product->current_quantity }}" />
+                                                                </td>
+                                                                <td>
+                                                                    <input type="number" name="qty[]"
+                                                                        class="form-control"
+                                                                        value="{{ $stock_count_product->updated_quantity }}"
+                                                                        required />
+                                                                </td>
+
+                                                            </tr>
+                                                        @endforeach
                                                     </tbody>
                                                 </table>
                                             </div>
@@ -115,121 +131,6 @@
         $("ul#product").siblings('a').attr('aria-expanded', 'true');
         $("ul#product").addClass("show");
         $("ul#product #stock-count-menu").addClass("active");
-
-        // array data depend on warehouse
-        var product_code = [];
-        var product_name = [];
-        var product_qty = [];
-
-        // array data with selection
-        var rowindex;
-        $('.selectpicker').selectpicker('refresh');
-
-
-        $('[data-toggle="tooltip"]').tooltip();
-
-        <?php $productArray = []; ?>
-        var lims_product_code = [
-            @foreach ($lims_product_list as $product)
-                <?php
-                $productArray[] = htmlspecialchars($product->code) . '|' . preg_replace('/[\n\r]/', '<br>', htmlspecialchars($product->name));
-                ?>
-            @endforeach
-            <?php
-            echo '"' . implode('","', $productArray) . '"';
-            ?>
-        ];
-
-        var lims_productcodeSearch = $('#lims_productcodeSearch');
-
-        lims_productcodeSearch.autocomplete({
-            source: function(request, response) {
-                var matcher = new RegExp(".?" + $.ui.autocomplete.escapeRegex(request.term), "i");
-                response($.grep(lims_product_code, function(item) {
-                    return matcher.test(item);
-                }));
-            },
-            response: function(event, ui) {
-                if (ui.content.length == 1) {
-                    var data = ui.content[0].value;
-                    $(this).autocomplete("close");
-                    productSearch(data);
-                };
-            },
-            select: function(event, ui) {
-                var data = ui.item.value;
-                productSearch(data);
-            }
-        });
-
-        //Delete product
-        $("table.order-list tbody").on("click", ".ibtnDel", function(event) {
-            rowindex = $(this).closest('tr').index();
-            $(this).closest("tr").remove();
-            calculateTotal();
-        });
-
-        function productSearch(data) {
-            $.ajax({
-                type: 'GET',
-                url: "{{ route('stock-count.search') }}",
-                data: {
-                    data: data
-                },
-                success: function(datas) {
-                    var flag = 1;
-                    var product_code = [];
-                    $(".product-code").each(function(i) {
-                        product_code.push($(this).val());
-                    });
-                    $("input[name='product_code_name']").val('');
-                    if (flag) {
-                        datas.forEach(function(data) {
-                            var newRow = $("<tr>");
-                            var cols = '';
-                            cols += '<td>' + data[0] +
-                                '</td>';
-                            cols += '<td>' + data[1] + '</td>';
-                            cols += '<td>' + data[2] + '</td>';
-                            cols +=
-                                '<td><input type="number" class="form-control qty" name="qty[]" value="0" step="any" required/></td>';
-                            cols +=
-                                '<td><button type="button" class="ibtnDel btn btn-md btn-danger">{{ trans('file.delete') }}</button></td>';
-                            cols +=
-                                '<input type="hidden" class="product-code" name="product_code[]" value="' +
-                                data[1] + '"/>';
-                            cols +=
-                                '<input type="hidden" class="current-qty" name="current_qty[]" value="' +
-                                data[2] + '"/>';
-                            cols +=
-                                '<input type="hidden" class="product-id" name="product_id[]" value="' +
-                                data[3] + '"/>';
-                            newRow.append(cols);
-                            $("table.order-list tbody").prepend(newRow);
-                            rowindex = newRow.index();
-                        });
-                    }
-                }
-            });
-        }
-
-        $(window).keydown(function(e) {
-            if (e.which == 13) {
-                var $targ = $(e.target);
-                if (!$targ.is("textarea") && !$targ.is(":button,:submit")) {
-                    var focusNext = false;
-                    $(this).find(":input:visible:not([disabled],[readonly]), a").each(function() {
-                        if (this === e.target) {
-                            focusNext = true;
-                        } else if (focusNext) {
-                            $(this).focus();
-                            return false;
-                        }
-                    });
-                    return false;
-                }
-            }
-        });
 
         $('#stock-count-form').on('submit', function(e) {
             var rownumber = $('table.order-list tbody tr:last').index();
