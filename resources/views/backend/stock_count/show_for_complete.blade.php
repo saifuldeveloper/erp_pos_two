bb343c9930044ecbbf74d72849d4b4c17876ab28
emon1432 <ovirajemon143@gmail.com>
2025-02-09 23:21:27 +0600

    Enhance stock count view: categorize stock items into matched, over, and under stock, and implement DataTable for improved display and interaction

--- resources/views/backend/stock_count/show_for_complete.blade.php
+++ resources/views/backend/stock_count/show_for_complete.blade.php
@@ -66,75 +66,93 @@
             @if (count($lims_stock_count->items) > 0)
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header d-flex align-items-center">
-                                Add List
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>{{ trans('file.name') }}</th>
-                                                <th>{{ trans('file.Code') }}</th>
-                                                <th>Current Quantity</th>
-                                                <th>Quantity Find</th>
-                                                <th>Remarks</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($lims_stock_count->items as $items)
-                                                {{-- @dd($item[0]->product->name) --}}
-                                                @php
-                                                    $item = $items[0];
-                                                @endphp
-                                                <tr>
-                                                    <td>{{ $item->product->name }}</td>
-                                                    <td>{{ $item->item_code }}</td>
-                                                    <td>{{ $item->current_quantity }}</td>
-                                                    <td>
-                                                        @php
-                                                            $total = 0;
-                                                        @endphp
-                                                        @foreach ($items as $item)
-                                                            {{ $item->updated_quantity }}
-                                                            @if (!$loop->last)
-                                                                +
-                                                            @endif
-                                                            @php
-                                                                $total += $item->updated_quantity;
-                                                            @endphp
-                                                        @endforeach
-                                                        = {{ $total }}
-                                                    </td>
-                                                    <td>
-                                                        @if ($total > $item->current_quantity)
-                                                            Over Stock ({{ $total - $item->current_quantity }})
-                                                        @elseif($total < $item->current_quantity)
-                                                            Under Stock ({{ $item->current_quantity - $total }})
-                                                        @else
-                                                            Stock Matched
-                                                        @endif
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                                {!! Form::open([
-                                    'route' => ['stock-count.update', $lims_stock_count->id],
-                                    'method' => 'put',
-                                    'files' => true,
-                                    'id' => 'stock-count-form',
-                                ]) !!}
-                                <input type="hidden" name="status" value="complete">
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary" id="submit-btn">Complete</button>
-                                </div>
-                                {!! Form::close() !!}
-                            </div>
-                        </div>
+                        @php
+                            $stockMatched = $lims_stock_count->items->filter(function ($items) {
+                                $total = $items->sum('updated_quantity');
+                                return $total == $items[0]->current_quantity;
+                            });
 
+                            $overStock = $lims_stock_count->items->filter(function ($items) {
+                                $total = $items->sum('updated_quantity');
+                                return $total > $items[0]->current_quantity;
+                            });
+
+                            $underStock = $lims_stock_count->items->filter(function ($items) {
+                                $total = $items->sum('updated_quantity');
+                                return $total < $items[0]->current_quantity;
+                            });
+
+                            $stockCounts = [
+                                ['title' => 'Stock Matched', 'data' => $stockMatched],
+                                ['title' => 'Over Stock', 'data' => $overStock],
+                                ['title' => 'Under Stock', 'data' => $underStock],
+                            ];
+                        @endphp
+                        @foreach ($stockCounts as $stockCount)
+                            @if ($stockCount['data']->count() > 0)
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <h5>{{ $stockCount['title'] }}</h5>
+                                            </div>
+                                        </div>
+                                        <div class="table-responsive">
+                                            <table class="table table-hover stock-count-table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>{{ trans('file.name') }}</th>
+                                                        <th>{{ trans('file.Code') }}</th>
+                                                        <th>Current Quantity</th>
+                                                        <th>Quantity Find</th>
+                                                        <th>Remarks</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($stockCount['data'] as $items)
+                                                        @php
+                                                            $item = $items[0];
+                                                            $total = $items->sum('updated_quantity');
+                                                        @endphp
+                                                        <tr>
+                                                            <td>{{ $item->product->name }}</td>
+                                                            <td>{{ $item->item_code }}</td>
+                                                            <td>{{ $item->current_quantity }}</td>
+                                                            <td>
+                                                                @foreach ($items as $item)
+                                                                    {{ $item->updated_quantity }}
+                                                                    @if (!$loop->last)
+                                                                        +
+                                                                    @endif
+                                                                @endforeach
+                                                                = {{ $total }}
+                                                            </td>
+                                                            <td>{{ $stockCount['title'] }}
+                                                                @if ($stockCount['title'] != 'Stock Matched')
+                                                                    ({{ abs($total - $item->current_quantity) }})
+                                                                @endif
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+
+                        {!! Form::open([
+                            'route' => ['stock-count.update', $lims_stock_count->id],
+                            'method' => 'put',
+                            'files' => true,
+                            'id' => 'stock-count-form',
+                        ]) !!}
+                        <input type="hidden" name="status" value="complete">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary" id="submit-btn">Complete</button>
+                        </div>
+                        {!! Form::close() !!}
                     </div>
                 </div>
             @endif
@@ -155,7 +173,6 @@
         // array data with selection
         var rowindex;
         $('.selectpicker').selectpicker('refresh');
-
 
         $('[data-toggle="tooltip"]').tooltip();
 
@@ -270,6 +287,20 @@
             } else {
                 $("#submit-btn").prop('disabled', true);
             }
+        });
+
+        $('.stock-count-table').DataTable({
+            "order": [],
+            'language': {
+                'lengthMenu': '_MENU_ {{ trans('file.records per page') }}',
+                "info": '<small>{{ trans('file.Showing') }} _START_ - _END_ (_TOTAL_)</small>',
+                "search": '{{ trans('file.Search') }}',
+                'paginate': {
+                    'previous': '<i class="dripicons-chevron-left"></i>',
+                    'next': '<i class="dripicons-chevron-right"></i>'
+                }
+            },
+            dom: '<"row"lfB>rtip',
         });
     </script>
 @endpush
