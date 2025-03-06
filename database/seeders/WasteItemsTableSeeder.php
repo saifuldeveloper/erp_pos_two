317591e878ef9ae2a3d7f16c2e14da160bd2626b
saifuldeveloper <mdsaiful77336@gmail.com>
2025-03-07 02:45:23 +0600

    seeder add

--- database/seeders/WasteItemsTableSeeder.php
+++ database/seeders/WasteItemsTableSeeder.php
@@ -18,6 +18,19 @@ class WasteItemsTableSeeder extends Seeder
 
         \DB::table('waste_items')->delete();
         
+        \DB::table('waste_items')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'waste_id' => 1,
+                'product_id' => 1023,
+                'qty' => 1,
+                'unit_price' => 1495.0,
+                'subtotal' => 1495.0,
+                'created_at' => '2025-03-07 05:35:28',
+                'updated_at' => '2025-03-07 05:35:28',
+            ),
+        ));
         
         
     }
--- database/seeders/WastesTableSeeder.php
+++ database/seeders/WastesTableSeeder.php
@@ -18,6 +18,20 @@ class WastesTableSeeder extends Seeder
 
         \DB::table('wastes')->delete();
         
+        \DB::table('wastes')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'receiver_type' => 'customer',
+                'receiver_id' => 970,
+                'receiver_name' => 'MITHU SB.',
+                'note' => 'Mithu saheb 1023 article 40 size er ekta niche',
+                'total_price' => '1495.00',
+                'status' => 1,
+                'created_at' => '2025-03-07 05:35:28',
+                'updated_at' => '2025-03-07 05:35:28',
+            ),
+        ));
         
         
     }
